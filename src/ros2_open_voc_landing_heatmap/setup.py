c0ee94ece16e34b705db8eb0de3a75927a65e0ab
ricardodeazambuja <ricardo.azambuja@gmail.com>
2023-08-03 16:19:50 -0400

    Added special config with gt semantic segmentation camera

--- src/ros2_open_voc_landing_heatmap/setup.py
+++ src/ros2_open_voc_landing_heatmap/setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+from glob import glob
 
 package_name = 'ros2_open_voc_landing_heatmap'
 
@@ -7,9 +8,10 @@ setup(
     version='0.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/cfg', glob('cfg/*.json')),
+        ('share/' + package_name, glob('launch/*.launch.py'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
