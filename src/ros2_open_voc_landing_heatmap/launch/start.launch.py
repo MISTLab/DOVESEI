2400e6533aa31a75f03fcd4548436a3ba2ae3eb0
ricardodeazambuja <ricardo.azambuja@gmail.com>
2023-08-09 19:20:12 -0400

    Cutting the shrubs

--- src/ros2_open_voc_landing_heatmap/launch/start.launch.py
+++ src/ros2_open_voc_landing_heatmap/launch/start.launch.py
@@ -19,7 +19,7 @@ negative_prompts = ["building, house, apartment-building, warehouse, shed, garag
                     "people, crowd", 
                     "vehicle, car, train", 
                     "lamp-post, transmission-line", 
-                    "fence, wall, hedgerow", "shrubs", 
+                    "fence, wall, hedgerow", 
                     "road, street, avenue, highway, drive, lane",
                     "stairs, steps, footsteps"]
 positive_prompts = ["grass, backyard, frontyard, courtyard, lawn", 
