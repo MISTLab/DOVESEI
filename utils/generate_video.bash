4b4e90c36218b9b7c774d64ba07c23c5e32b996c
ricardodeazambuja <ricardo.azambuja@gmail.com>
2023-08-15 14:37:39 -0400

    Script to generate a video from saved images

---
+++ utils/generate_video.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+experiment_folders=($(ls -d experiment_*)) # create the experiment name
+
+for i in "${!experiment_folders[@]}"
+do
+  echo "Processing folder ${experiment_folders[i]}"
+  cd ${experiment_folders[i]}
+  ls rgb* | grep png | xargs cat | ffmpeg -f image2pipe -i - ${experiment_folders[i]}.mp4
+  cd ..
+done
