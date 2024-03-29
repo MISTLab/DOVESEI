891b0f116c279efb19a53d07d28399365135f9dc
Haechan Mark Bong <haechan.bong@gmail.com>
2024-03-29 14:43:30 -0400

    Update test_copyright.py

--- src/ros2_open_voc_landing_heatmap/test/test_copyright.py
+++ src/ros2_open_voc_landing_heatmap/test/test_copyright.py
@@ -15,9 +15,8 @@
 from ament_copyright.main import main
 import pytest
 
-
 @pytest.mark.copyright
 @pytest.mark.linter
 def test_copyright():
-    rc = main(argv=['.', 'test'])
+    rc = main(argv = ['.', 'test'])
     assert rc == 0, 'Found errors'
