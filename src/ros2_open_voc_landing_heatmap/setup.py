f0d0a7afd9c34295cdd6b88b142daf176ba5daf6
Haechan Mark Bong <haechan.bong@gmail.com>
2024-03-29 14:42:09 -0400

    Update setup.py

--- src/ros2_open_voc_landing_heatmap/setup.py
+++ src/ros2_open_voc_landing_heatmap/setup.py
@@ -5,22 +5,22 @@ package_name = 'ros2_open_voc_landing_heatmap'
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=[package_name],
-    data_files=[
+    version = '0.0.0',
+    packages = [package_name],
+    data_files = [
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/cfg', glob('cfg/*.json')),
         ('share/' + package_name, glob('launch/*.launch.py'))
     ],
-    install_requires=['setuptools'],
-    zip_safe=True,
-    maintainer='ros2user',
-    maintainer_email='ros2user@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
-    entry_points={
+    install_requires = ['setuptools'],
+    zip_safe = True,
+    maintainer = 'ros2user',
+    maintainer_email = 'haechan.bong@polymtl.ca',
+    description = 'DOVESEI',
+    license = 'TODO: License declaration',
+    tests_require = ['pytest'],
+    entry_points = {
         'console_scripts': [
             'getlandingheatmap_service = ros2_open_voc_landing_heatmap.generate_landing_heatmap:main',
             'lander_publisher = ros2_open_voc_landing_heatmap.lander_publisher:main',
