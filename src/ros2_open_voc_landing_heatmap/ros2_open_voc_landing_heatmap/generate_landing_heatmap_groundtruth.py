b15dc9c192aea1f0f7ad3fa3812f2523ac376a40
Haechan Mark Bong <haechan.bong@gmail.com>
2024-03-29 14:52:13 -0400

    Update generate_landing_heatmap_groundtruth.py

--- src/ros2_open_voc_landing_heatmap/ros2_open_voc_landing_heatmap/generate_landing_heatmap_groundtruth.py
+++ src/ros2_open_voc_landing_heatmap/ros2_open_voc_landing_heatmap/generate_landing_heatmap_groundtruth.py
@@ -2,7 +2,6 @@ from time import sleep
 import numpy as np
 import cv2
 from PIL import Image
-
 import torch
 from transformers import CLIPSegProcessor, CLIPSegForImageSegmentation
 processor = CLIPSegProcessor.from_pretrained("CIDAS/clipseg-rd64-refined") 
@@ -11,10 +10,8 @@ if torch.cuda.is_available():
 else:
     model = CLIPSegForImageSegmentation.from_pretrained("CIDAS/clipseg-rd64-refined")
 
-
 from ros2_open_voc_landing_heatmap_srv.srv import GetLandingHeatmap
 import rclpy
-# from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy
 from rclpy.node import Node
 from sensor_msgs.msg import Image as ImageMsg
 from sensor_msgs.msg import Imu
@@ -64,13 +61,12 @@ class GenerateLandingHeatmap(Node):
             self.start_node,
             1)
 
-
     def start_node(self, msg):
         self.get_logger().warn('Simulator is online!')
-        self.destroy_subscription(self.check_flying_sensor_alive) # we don't need this subscriber anymore...
+        self.destroy_subscription(self.check_flying_sensor_alive) # We don't need this subscriber anymore.
 
         self.last_img = None
-        self.final_img_pub = self.create_publisher(ImageMsg, "/final_heatmap",1) ##DEBUG
+        self.final_img_pub = self.create_publisher(ImageMsg, "/final_heatmap",1) # DEBUG
 
         self.img_sub = self.create_subscription(
             ImageMsg,
@@ -80,14 +76,13 @@ class GenerateLandingHeatmap(Node):
         self.srv = self.create_service(GetLandingHeatmap, 'generate_landing_heatmap', self.get_landing_heatmap_callback)
         self.get_logger().info('generate_landing_heatmap is up and running!')
         
-
     def img_sub_cb(self, msg):
-            self.last_img = cv2.resize(self.cv_bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8'),(CLIPSEG_OUTPUT_SIZE,CLIPSEG_OUTPUT_SIZE))
+            self.last_img = cv2.resize(self.cv_bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8'), (CLIPSEG_OUTPUT_SIZE,CLIPSEG_OUTPUT_SIZE))
 
     @staticmethod
     def get_mask(img, label):
-        # img is BGR!
-        return (img[...,2] == LABELS[label][0]) & (img[...,1] == LABELS[label][1]) & (img[...,0] == LABELS[label][2])
+        # img. is BGR!
+        return (img[...,2] == LABELS[label][0]) & (img[..., 1] == LABELS[label][1]) & (img[..., 0] == LABELS[label][2])
     
     def get_landing_heatmap_callback(self, request, response):
         # Inputs: request.image, request.negative_prompts, request.positive_prompts, request.prompt_engineering
@@ -105,40 +100,38 @@ class GenerateLandingHeatmap(Node):
         input_image = self.cv_bridge.imgmsg_to_cv2(input_image_msg, desired_encoding='rgb8')
         safety_threshold = request.safety_threshold
         blur_kernel_size = request.blur_kernel_size
-        
-       
-        # places to land mask
-        logits = np.ones(self.last_img.shape[:2])==0
+    
+        # Places to land mask
+        logits = np.ones(self.last_img.shape[:2]) == 0
         for label in PLACES2LAND:
             logits |= self.get_mask(self.last_img, label)
         logits = logits.astype('float32')
 
-        kernel = np.ones((3,3),np.uint8)
-        logits = cv2.erode(logits,kernel,iterations=2)
+        kernel = np.ones((3, 3), np.uint8)
+        logits = cv2.erode(logits,kernel,iterations = 2)
 
         # Blur
-        logits = cv2.blur(logits,(blur_kernel_size, blur_kernel_size))
+        logits = cv2.blur(logits, (blur_kernel_size, blur_kernel_size))
 
-        # Finally, resize to match input image
-        logits = cv2.resize(logits, (input_image.shape[1],input_image.shape[0]), cv2.INTER_AREA)
+        # Resize to match input image
+        logits = cv2.resize(logits, (input_image.shape[1], input_image.shape[0]), cv2.INTER_AREA)
 
-        logits = logits > 0.2
+        logits = logits > .2
         response.success = False
         if request.dynamic_threshold > 0.0:
             total_pixels = np.prod(logits.shape)
-            if not ((logits==True).sum()/total_pixels < request.dynamic_threshold):
+            if not ((logits==True).sum() / total_pixels < request.dynamic_threshold):
                 response.success = True
         else:
             response.success = True
         
-        # returns heatmap as grayscale image
-        response.heatmap = self.cv_bridge.cv2_to_imgmsg((logits*255).astype('uint8'), encoding='mono8')
+        # Returns heatmap as grayscale image
+        response.heatmap = self.cv_bridge.cv2_to_imgmsg((logits * 255).astype('uint8'), encoding = 'mono8')
         response.heatmap.header.frame_id = input_image_msg.header.frame_id
 
-        self.final_img_pub.publish(response.heatmap)##DEBUG
+        self.final_img_pub.publish(response.heatmap) # DEBUG
 
         return response
-
 
 def main():
     rclpy.init()
@@ -152,7 +145,6 @@ def main():
     finally:
         generate_landing_heatmap.get_logger().info(f'Shutting generate_landing_heatmap down...')
         rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
